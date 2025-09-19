439565bdc4a59c62572de3ea9dc7c623249c009d
Shantanu Borade <Shantanuborade38@gmail.com>
2025-09-19 18:05:30 +0530

    improvement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 export default function ProductPage() {
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50 pt-16">
+    <div className="flex flex-col min-h-screen bg-gray-50">
       <Navbar />
 
   
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -17,7 +17,7 @@ export default function ProfilePage() {
   ];
 
   return (
-    <main className="min-h-screen flex flex-col bg-gray-50 pt-16">
+    <main className="min-h-screen flex flex-col bg-gray-50">
       <Navbar />
 
       <div className="flex flex-1 p-6 gap-6">
